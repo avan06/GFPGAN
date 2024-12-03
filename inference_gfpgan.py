3b3cd9185841affbed2819e57218d21e58dc1af8
avan <sakaja_218@yahoo.com.tw>
2024-12-04 00:32:59 +0800

    Added support for the '-sub' and '--search_subfolders' options,
    which, when executed, will search for subfolders within the provided input path.

--- inference_gfpgan.py
+++ inference_gfpgan.py
@@ -22,7 +22,7 @@ def main():
     parser.add_argument('-o', '--output', type=str, default='results', help='Output folder. Default: results')
     # we use version to select models, which is more user-friendly
     parser.add_argument(
-        '-v', '--version', type=str, default='1.3', help='GFPGAN model version. Option: 1 | 1.2 | 1.3. Default: 1.3')
+        '-v', '--version', type=str, default='1.4', help='GFPGAN model version. Option: 1 | 1.2 | 1.3 | 1.4. Default: 1.4')
     parser.add_argument(
         '-s', '--upscale', type=float, default=2, help='The final upsampling scale of the image. Default: 2')
 
@@ -36,6 +36,7 @@ def main():
     parser.add_argument('--suffix', type=str, default=None, help='Suffix of the restored faces')
     parser.add_argument('--only_center_face', action='store_true', help='Only restore the center face')
     parser.add_argument('--aligned', action='store_true', help='Input are aligned faces')
+    parser.add_argument('-sub', '--search_subfolders', action='store_true', help='Decide whether to search subfolders.')
     parser.add_argument(
         '--ext',
         type=str,
@@ -123,11 +124,25 @@ def main():
         bg_upsampler=bg_upsampler)
 
     # ------------------------ restore ------------------------
+    processing(args, restorer, img_list)
+
+    print(f'Results are in the [{args.output}] folder.')
+
+
+def processing(args, restorer, img_list: list[str], folder_name: str =""):
+    if folder_name:
+        print(f"\n________________________________________________\nCurrently processing the subfolder: {folder_name}\n________________________________________________\n")
+
     imgsLen = len(img_list)
     for idx, img_path in enumerate(img_list):
         # read image
         try:
             img_name = os.path.basename(img_path)
+            if os.path.isdir(img_path) and args.search_subfolders:
+                print(f"path {img_path}")
+                processing(args, restorer, sorted(glob.glob(os.path.join(img_path, '*'))), img_name)
+                continue
+
             print(f'[{idx}/{imgsLen}] Processing {img_name} ...')
             basename, ext = os.path.splitext(img_name)
             input_img = cv2.imdecode(np.fromfile(img_path, np.uint8), cv2.IMREAD_COLOR) #input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
@@ -138,46 +153,47 @@ def main():
         except Exception as err:
             print("  cv2.imread result Exception: " + str(err))
             continue
+        restorer_enhance(args, restorer, idx, input_img, basename, ext, folder_name)
 
-        # restore faces and background if necessary
-        cropped_faces, restored_faces, restored_img = restorer.enhance(
-            input_img,
-            has_aligned=args.aligned,
-            only_center_face=args.only_center_face,
-            paste_back=True,
-            weight=args.weight)
 
-        # save faces
-        for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
-            # save cropped face
-            save_crop_path = os.path.join(args.output, 'cropped_faces', f'{basename}_{idx:02d}.png')
-            imwriteUTF8(cropped_face, save_crop_path, '.png') #imwrite(cropped_face, save_crop_path)
-            # save restored face
-            if args.suffix is not None:
-                save_face_name = f'{basename}_{idx:02d}_{args.suffix}.png'
-            else:
-                save_face_name = f'{basename}_{idx:02d}.png'
-            save_restore_path = os.path.join(args.output, 'restored_faces', save_face_name)
-            imwriteUTF8(restored_face, save_restore_path, '.png') #imwrite(restored_face, save_restore_path)
-            
-            # save comparison image
-            cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
-            imwriteUTF8(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'), '.png') #imwrite(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'))
-
-        # save restored img
-        if restored_img is not None:
-            if args.ext == 'auto':
-                extension = ext[1:]
-            else:
-                extension = args.ext
-
-            if args.suffix is not None:
-                save_restore_path = os.path.join(args.output, 'restored_imgs', f'{basename}_{args.suffix}.{extension}')
-            else:
-                save_restore_path = os.path.join(args.output, 'restored_imgs', f'{basename}.{extension}')
-            imwriteUTF8(restored_img, save_restore_path, f'.{extension}') #imwrite(restored_img, save_restore_path)
-
-    print(f'Results are in the [{args.output}] folder.')
+def restorer_enhance(args, restorer: GFPGANer, idx: int, input_img: cv2.typing.MatLike, basename: str, ext: str, folder_name: str =""):
+    # restore faces and background if necessary
+    cropped_faces, restored_faces, restored_img = restorer.enhance(
+        input_img,
+        has_aligned=args.aligned,
+        only_center_face=args.only_center_face,
+        paste_back=True,
+        weight=args.weight)
+    
+    # save faces
+    for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
+        # save cropped face
+        save_crop_path = os.path.join(args.output, folder_name, 'cropped_faces', f'{basename}_{idx:02d}.png')
+        imwriteUTF8(cropped_face, save_crop_path, '.png') #imwrite(cropped_face, save_crop_path)
+        # save restored face
+        if args.suffix is not None:
+            save_face_name = f'{basename}_{idx:02d}_{args.suffix}.png'
+        else:
+            save_face_name = f'{basename}_{idx:02d}.png'
+        save_restore_path = os.path.join(args.output, folder_name, 'restored_faces', save_face_name)
+        imwriteUTF8(restored_face, save_restore_path, '.png') #imwrite(restored_face, save_restore_path)
+        
+        # save comparison image
+        cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
+        imwriteUTF8(cmp_img, os.path.join(args.output, folder_name, 'cmp', f'{basename}_{idx:02d}.png'), '.png') #imwrite(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'))
+    
+    # save restored img
+    if restored_img is not None:
+        if args.ext == 'auto':
+            extension = ext[1:]
+        else:
+            extension = args.ext
+    
+        if args.suffix is not None:
+            save_restore_path = os.path.join(args.output, folder_name, 'restored_imgs', f'{basename}_{args.suffix}.{extension}')
+        else:
+            save_restore_path = os.path.join(args.output, folder_name, 'restored_imgs', f'{basename}.{extension}')
+        imwriteUTF8(restored_img, save_restore_path, f'.{extension}') #imwrite(restored_img, save_restore_path)
 
 def imwriteUTF8(image, save_path, extension):
     is_success, im_buf_arr = cv2.imencode(extension, image)
